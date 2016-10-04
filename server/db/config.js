aec74fc70495c054ba614bcaecb36c384bf6b47e
Frank Grimes <fg9@certifiedesigns.com>
2016-10-04 12:04:18 -0700

    fixed db credentials

--- server/db/config.js
+++ server/db/config.js
@@ -5,5 +5,5 @@ var crypto = require('crypto');
 
 module.exports = {
     'secret': "DC368AC3FD182BAE209312D9158AE2E5BC96F5EA05AED069A4226BF0B1A4D3F6",
-    'database': 'mongodb://heroku_vj01pz6p:jadajv123@ds049476.mlab.com:49476/heroku_vj01pz6p'
+    'database': 'mongodb://heroku_vj01pz6p:o1gauhdsg1dheplhvrq7nb0fef@ds049476.mlab.com:49476/heroku_vj01pz6p'
 };
